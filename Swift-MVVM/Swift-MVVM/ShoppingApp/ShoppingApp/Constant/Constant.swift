82db3e5fd034d168bc8085adc85a76c150cad7e0
Surendra Sheoran <119883716+Surendrasheoran@users.noreply.github.com>
2023-01-29 19:05:13 +0530

    Add files via upload

---
+++ Swift-MVVM/Swift-MVVM/ShoppingApp/ShoppingApp/Constant/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  ShoppingApp
+//
+//  Created by Surendra Kumar on 28/01/23.
+//
+
+enum Constant{
+    enum API{
+        static let productURL = "https://fakestoreapi.com/products"
+    }
+    
+}
---
+++ Swift-MVVM/Swift-MVVM/ShoppingApp/ShoppingApp/Screens/Product/View/ProductTVC.swift
@@ -0,0 +1,58 @@
+//
+//  ProductTVC.swift
+//  ShoppingApp
+//
+//  Created by Surendra Kumar on 29/01/23.
+//
+
+import UIKit
+
+class ProductTVC: UITableViewCell {
+
+    @IBOutlet weak var productCellBG: UIView!
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    
+    @IBOutlet weak var productRate: UIButton!
+    @IBOutlet weak var productSubTitle: UILabel!
+    
+    @IBOutlet weak var productDescription: UILabel!
+    
+    @IBOutlet weak var productPrice: UILabel!
+    
+    var product: Product?
+    {
+        didSet{
+            productConfiguration()
+        }
+    }
+    
+    @IBOutlet weak var buyButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    func productConfiguration()  {
+        guard product != nil else
+        {
+            return;
+        }
+        
+        productTitle.text = product?.title
+        productPrice.text = "$\(product?.price.description ?? "0")"
+        productRate.setTitle("\(product?.rating.rate.description ?? "5")", for: UIControl.State.normal)
+        productSubTitle.text = product?.category
+        productDescription.text = product?.description
+        productImage.setImage(urlString: product!.image)
+        productImage.layer.cornerRadius = 10
+        productImage.clipsToBounds = true
+    }
+}
